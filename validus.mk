b43fbfebf1038ab105c64e756ee2062f1863d5c1
Anthony Pyrtle <xxpyrtlexx93@gmail.com>
2016-02-16 12:48:53 -0500

    Vali-fy

--- validus.mk
+++ validus.mk
@@ -17,9 +17,9 @@
 $(call inherit-product, device/htc/m8/full_m8.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/tesla/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/validus/config/nfc_enhanced.mk)
 
-# Inherit some common Tesla stuff.
-$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
+# Inherit some common ValidusOs stuff.
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
 
-PRODUCT_NAME := tesla_m8
+PRODUCT_NAME := validus_m8
